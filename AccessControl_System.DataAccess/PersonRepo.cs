9776f1a9465127867a759f8be10b0425f4cde825
Sonyxe <61245381+Sonyxe@users.noreply.github.com>
2022-04-20 06:14:02 -0400

    Add files via upload

---
+++ AccessControl_System.DataAccess/PersonRepo.cs
@@ -0,0 +1,11 @@
+﻿using Access_control_system.Models;
+using System;
+
+
+namespace AccessControl_System.DataAccess
+{
+    public class PersonRepo
+    {
+        Person person = new Person();
+    }
+}
